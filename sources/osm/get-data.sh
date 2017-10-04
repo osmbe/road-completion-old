2b2562a8745cbeecc9d4dea611d930b0f36b0136
xivk <ben@itinero.tech>
2017-10-04 11:52:58 +0200

    Keep all the highways.

--- sources/osm/get-data.sh
+++ sources/osm/get-data.sh
@@ -5,7 +5,7 @@
 wget http://download.geofabrik.de/europe/belgium-latest.osm.pbf  -O ./belgium-latest.osm.pbf
 
 #convert to geojson while taking only highways.
-ogr2ogr -f GeoJSON -select name,highway -where "highway is not null AND highway NOT IN ( 'pedestrian', 'cycleway', 'path')"  -nln osm_highways -progress  belgium.geojson  belgium-latest.osm.pbf  lines
+ogr2ogr -f GeoJSON -select name,highway -where "highway is not null"  -nln osm_highways -progress  belgium.geojson  belgium-latest.osm.pbf  lines
 
 #convert to vectortiles
 # TODO: test with zoom-level limits, we only need level 14.
