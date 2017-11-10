9a7df69df71e83c615e7b4e308fb7b3add472cd2
Ben Abelshausen <ben.abelshausen@gmail.com>
2017-11-10 15:38:03 +0000

    Update process-data.sh

--- sources/osm/timeseries/process-data.sh
+++ sources/osm/timeseries/process-data.sh
@@ -36,14 +36,14 @@ if [ ! -f ./jaar2017.geojson ]; then
 fi
 
 
-tippecanoe -o ./flanders-year-2007.mbtiles ./jaar-2007.geojson -l roads -f -pf -pk
-tippecanoe -o ./flanders-year-2008.mbtiles ./jaar-2008.geojson -l roads -f -pf -pk
-tippecanoe -o ./flanders-year-2009.mbtiles ./jaar-2009.geojson -l roads -f -pf -pk
-tippecanoe -o ./flanders-year-2010.mbtiles ./jaar-2010.geojson -l roads -f -pf -pk
-tippecanoe -o ./flanders-year-2011.mbtiles ./jaar-2011.geojson -l roads -f -pf -pk
-tippecanoe -o ./flanders-year-2012.mbtiles ./jaar-2012.geojson -l roads -f -pf -pk
-tippecanoe -o ./flanders-year-2013.mbtiles ./jaar-2013.geojson -l roads -f -pf -pk
-tippecanoe -o ./flanders-year-2014.mbtiles ./jaar-2014.geojson -l roads -f -pf -pk
-tippecanoe -o ./flanders-year-2015.mbtiles ./jaar-2015.geojson -l roads -f -pf -pk
-tippecanoe -o ./flanders-year-2016.mbtiles ./jaar-2016.geojson -l roads -f -pf -pk
-tippecanoe -o ./flanders-year-2017.mbtiles ./jaar-2017.geojson -l roads -f -pf -pk
+tippecanoe -o ./flanders-year-2007.mbtiles ./jaar2007.geojson -l roads -f -pf -pk
+tippecanoe -o ./flanders-year-2008.mbtiles ./jaar2008.geojson -l roads -f -pf -pk
+tippecanoe -o ./flanders-year-2009.mbtiles ./jaar2009.geojson -l roads -f -pf -pk
+tippecanoe -o ./flanders-year-2010.mbtiles ./jaar2010.geojson -l roads -f -pf -pk
+tippecanoe -o ./flanders-year-2011.mbtiles ./jaar2011.geojson -l roads -f -pf -pk
+tippecanoe -o ./flanders-year-2012.mbtiles ./jaar2012.geojson -l roads -f -pf -pk
+tippecanoe -o ./flanders-year-2013.mbtiles ./jaar2013.geojson -l roads -f -pf -pk
+tippecanoe -o ./flanders-year-2014.mbtiles ./jaar2014.geojson -l roads -f -pf -pk
+tippecanoe -o ./flanders-year-2015.mbtiles ./jaar2015.geojson -l roads -f -pf -pk
+tippecanoe -o ./flanders-year-2016.mbtiles ./jaar2016.geojson -l roads -f -pf -pk
+tippecanoe -o ./flanders-year-2017.mbtiles ./jaar2017.geojson -l roads -f -pf -pk
