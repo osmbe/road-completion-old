d9bb52e0a3cbc898b8a7e1ff9a1d0104b90be34b
xivk <ben@itinero.tech>
2017-11-11 19:04:53 +0100

    Updated tile-reduce.

--- node_modules/@mapbox/tile-reduce/examples/count/count.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function(data, tile, writeData, done) {
-  var count = 0;
-  if (data.osm.roads) count += data.osm.roads.length;
-  if (data.osm.buildings) count += data.osm.buildings.length;
-  done(null, count);
-};
--- node_modules/@mapbox/tile-reduce/test/fixtures/count.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function(data, tile, writeData, done) {
-  var count = 0;
-  if (data.osm.roads) count += data.osm.roads.length;
-  if (data.osm.buildings) count += data.osm.buildings.length;
-  if (data.tiger.tiger) count += data.tiger.tiger.length;
-  done(null, count);
-};
