e2f87e5d3fee64dd0da13365392b4dad1fa0eb2a
noname <andrewsleruth@gmail.com>
2018-07-23 09:58:27 +0200

    Added clear comments to the urbis tag processing script

--- docker/tagprocess/processUrbis.js
+++ docker/tagprocess/processUrbis.js
@@ -1,3 +1,7 @@
+// This is the modified version of the process.js file
+// process.js converts from wegenregister to OSM tags
+// this one converts from urbis to OSM tags
+
 var geojsonTransform = require("./geojson-transform");
 
 var args = process.argv.slice(2);
@@ -22,6 +26,12 @@ geojsonTransform.transform(opts.source, opts.target, function(p) {
         console.log("Processed " + i + " linestrings...");
     }
 
+    // Check if the "TYPE" property exists
+    // These are the different urbis types I found the most useful (that have an OSM tag equivalent)
+    // TYPE K: parking aisles
+    // TYPE B: bridges
+    // TYPE T: tunnels
+    // TYPE S, I, W: road types (just unclassified highway in OSM)
     if (p.TYPE)
     {
         if (p.TYPE == 'K')
@@ -49,6 +59,7 @@ geojsonTransform.transform(opts.source, opts.target, function(p) {
         }
     }
 
+    // I had to comment this part out because I couldn't seem to access the fr and nl name inside of "other_tags"
     /*if (p.PN_NAME_FR && p.PN_NAME_FR != 'Inconnu')
     {
         transformed.other_tags["name"]["fr"] = p.PN_NAME_FR;
@@ -59,25 +70,32 @@ geojsonTransform.transform(opts.source, opts.target, function(p) {
         transformed.other_tags["name"]["nl"] = p.PN_NAME_DU;
     }*/
 
+    // Check if the name (FR or NL) property exists
     if (p.PN_NAME_FR && p.PN_NAME_DU) 
     {
+        // We don't want to convert to osm tags if the name appears as "unknown"
         if (p.PN_NAME_DU != 'Ongekend' && p.PN_NAME_FR != 'Inconnu')
         {
-
+            // If the FR and NL tags are the same value, we just add one name
             if (p.PN_NAME_FR == p.PN_NAME_DU) 
             {
                 transformed.name = p.PN_NAME_FR;
             }
-
-            transformed.name = p.PN_NAME_FR + " - " + p.PN_NAME_DU;
+            // else we add both names
+            else
+            {
+                transformed.name = p.PN_NAME_FR + " - " + p.PN_NAME_DU;
+            } 
         }
     }
 
+    // We check if the postal code property exists inside of Urbis
     if (p.PZ_NAT_COD)
     {
         transformed.postal_code = p.PZ_NAT_COD;
     }
 
+    // We add the source
     transformed.source = "urbis";
 
     if (transformed.highway) 
