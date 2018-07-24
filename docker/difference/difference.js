5fded6aff5a6cdb90e9666bb46b954888901bd96
noname <andrewsleruth@gmail.com>
2018-07-24 09:54:05 +0200

    Code is more readable now

--- docker/difference/difference.js
+++ docker/difference/difference.js
@@ -6,7 +6,8 @@ var turf = require('@turf/turf'),
   hashF = require('object-hash'),
   fx = require('mkdir-recursive');
 
-module.exports = function(data, tile, writeData, done) {
+module.exports = (data, tile, writeData, done) => 
+{
   var refDeltas = turf.featureCollection([]);
   var streetBuffers = undefined;
   var refRoads = undefined;
@@ -14,11 +15,13 @@ module.exports = function(data, tile, writeData, done) {
   var refRoadsLength = 0;
   var diffRoadsLength = 0;
   var debugDir = "/home/xivk/work/osmbe/road-completion/debug/";
-  if (!fs.existsSync(debugDir)) {
+  if (!fs.existsSync(debugDir)) 
+  {
     debugDir = undefined;
   }
 
-  try {
+  try 
+  {
     if (tile[2] == 14)
     {
       var tileDir = tile[2] + "/" + tile[0] + "/";
@@ -27,27 +30,34 @@ module.exports = function(data, tile, writeData, done) {
       var refRoadsDir = debugDir + "refroads/" + tileDir;
       var osmBuffersDir = debugDir + "osmbuffers/" + tileDir;
       var diffsDir = debugDir + "diffs/" + tileDir;
-      if (debugDir) {
-        if (!fs.existsSync(osmDataDir)){
+      if (debugDir) 
+      {
+        if (!fs.existsSync(osmDataDir))
+        {
           fx.mkdirSync(osmDataDir);
         }
-        if (!fs.existsSync(refRoadsDir)){
+        if (!fs.existsSync(refRoadsDir))
+        {
           fx.mkdirSync(refRoadsDir);
         }
-        if (!fs.existsSync(osmBuffersDir)){
+        if (!fs.existsSync(osmBuffersDir))
+        {
           fx.mkdirSync(osmBuffersDir);
         }
-        if (!fs.existsSync(diffsDir)){
+        if (!fs.existsSync(diffsDir))
+        {
           fx.mkdirSync(diffsDir);
         }
       }
       
       // concat feature classes and normalize data
       refRoads = normalize(data.ref.roads);
-      if (data.source) {
+      if (data.source) 
+      {
         var osmData = normalize(data.source.roads);
 
-        if (debugDir) {
+        if (debugDir) 
+        {
           fs.writeFile (osmDataDir + tileName, JSON.stringify(osmData));
           fs.writeFile (refRoadsDir + tileName, JSON.stringify(refRoads));
         }
@@ -83,27 +93,38 @@ module.exports = function(data, tile, writeData, done) {
           fs.writeFile (osmBuffersDir + tileName, JSON.stringify(merged));
         }
 
-        if (refRoads && streetBuffers) {
-          refRoads.features.forEach(function(refRoad){
-            streetBuffers.features.forEach(function(streetsRoad){
+        if (refRoads && streetBuffers) 
+        {
+          refRoads.features.forEach((refRoad) =>
+          {
+            streetBuffers.features.forEach((streetsRoad) => 
+            {
               var roadDiff = turf.difference(refRoad, streetsRoad);
               refRoadType = refRoad.geometry.type;
-              if(roadDiff && !filter(roadDiff, refRoadType)){
-                for(var c = 0; c < refRoad.geometry.coordinates.length; c++){
-                  for(var d = 0; d < refRoad.geometry.coordinates[c].length; d++){
+              if(roadDiff && !filter(roadDiff, refRoadType))
+              {
+                for(var c = 0; c < refRoad.geometry.coordinates.length; c++)
+                {
+                  for(var d = 0; d < refRoad.geometry.coordinates[c].length; d++)
+                  {
                     refRoadsLength += turf.length(refRoad.geometry.coordinates[c][d]);
                   }
                 }
-                for(var e = 0; e < roadDiff.geometry.coordinates.length; e++){
-                  for(var f = 0; f <roadDiff.geometry.coordinates[e].length; f++){
-                  diffRoadsLength += turf.length(roadDiff.geometry.coordinates[e][f]);
+                for(var e = 0; e < roadDiff.geometry.coordinates.length; e++)
+                {
+                  for(var f = 0; f <roadDiff.geometry.coordinates[e].length; f++)
+                  {
+                    diffRoadsLength += turf.length(roadDiff.geometry.coordinates[e][f]);
                   }
                 }
                 // Compare to see if there is a difference in their names
                   
-                  if( refRoad.geometry.type === "Polygon" && CompareByTags(refRoad.properties.name, streetsRoad.properties.name)) {
-                    if(refRoad.properties.bridge || refRoad.properties.tunnel || refRoad.properties.cobblestone) {
-                      if(!CompareByTag(streetsRoad.properties.bridge) || !CompareByTag(streetsRoad.properties.tunnel) || !CompareCobblestone(streetsRoad.properties.surface)) {
+                  if( refRoad.geometry.type === "Polygon" && CompareByTags(refRoad.properties.name, streetsRoad.properties.name)) 
+                  {
+                    if(refRoad.properties.bridge || refRoad.properties.tunnel || refRoad.properties.cobblestone) 
+                    {
+                      if(!CompareByTag(streetsRoad.properties.bridge) || !CompareByTag(streetsRoad.properties.tunnel) || !CompareCobblestone(streetsRoad.properties.surface)) 
+                      {
                         refDeltas.features.push(roadDiff);
                       } 
                     }
@@ -114,15 +135,17 @@ module.exports = function(data, tile, writeData, done) {
             });
           });
         }
-      } else {
+      } else 
+      {
         refDeltas = refRoads;
       }
 
       // add hashes as id's and tile-id's.
-      for (var f = 0; f < refDeltas.features.length; f++) {
+      for (var f = 0; f < refDeltas.features.length; f++) 
+      {
         var feature = refDeltas.features[f];
-        if (feature &&
-            feature.geometry) {
+        if (feature && feature.geometry) 
+        {
 
             // 07/09** I think we could hash the tiles too (property part of feature)
             //hash = hashF(feature);
@@ -137,32 +160,43 @@ module.exports = function(data, tile, writeData, done) {
             var coords = 0;
             var hash = undefined;
 
-            try {
-              if(feature.geometry.type === "Polygon") {
-                for(var c = 0; c < feature.geometry.coordinates.length; c++){
-                  for(var d = 0; d < feature.geometry.coordinates[c].length; d++){
-                    for(var e = 0; e < feature.geometry.coordinates[c][d].length; e++){
+            try 
+            {
+              if(feature.geometry.type === "Polygon") 
+              {
+                for(var c = 0; c < feature.geometry.coordinates.length; c++)
+                {
+                  for(var d = 0; d < feature.geometry.coordinates[c].length; d++)
+                  {
+                    for(var e = 0; e < feature.geometry.coordinates[c][d].length; e++)
+                    {
                       coords += feature.geometry.coordinates[c][d][e];
                     }
                   }
                 }
               }
-              else {
-                for(var c = 0; c < feature.geometry.coordinates.length; c++){
-                  for(var d = 0; d < feature.geometry.coordinates[c].length;d++){
+              else 
+              {
+                for(var c = 0; c < feature.geometry.coordinates.length; c++)
+                {
+                  for(var d = 0; d < feature.geometry.coordinates[c].length;d++)
+                  {
                     coords += feature.geometry.coordinates[c][d];
                   }
                 }
               }
               hash = getNewHash( hashF(feature.properties), hashF(coords));
-              if(hash === null || hash === "") {
+              if(hash === null || hash === "") 
+              {
                 throw new NotConfirmedHashCodeException();
-            }
-
-            }catch(err) {
-                err = new NotConfirmedHashCodeException("Impossible to hash coordinates and properties");
-                console.log(err.toString());
               }
+
+            }
+            catch(err) 
+            {
+              err = new NotConfirmedHashCodeException("Impossible to hash coordinates and properties");
+              console.log(err.toString());
+            }
 
           feature.properties.id = "" + hash;
           feature.properties.tile_z = tile[2];
@@ -171,7 +205,8 @@ module.exports = function(data, tile, writeData, done) {
         }
       }
 
-      if (debugDir) {
+      if (debugDir) 
+      {
         fs.writeFile (diffsDir + tileName, JSON.stringify(normalize(refDeltas)));
       }
     }
@@ -195,51 +230,69 @@ module.exports = function(data, tile, writeData, done) {
    });
 };
 
-function clip(lines, tile) {
-  lines.features = lines.features.map(function(line){
-    try {
+function clip(lines, tile) 
+{
+  lines.features = lines.features.map(function(line)
+  {
+    try 
+    {
       var clipped = turf.intersect(line, turf.polygon(tilebelt.tileToGeoJSON(tile).coordinates));
       return clipped;
-    } catch(e){
+    } 
+    catch(e)
+    {
       return;
     }
   });
-  lines.features = lines.features.filter(function(line){
+  lines.features = lines.features.filter((line) => 
+  {
     if(line) return true;
   });
-  lines.features = lines.features.filter(function(line){
+  lines.features = lines.features.filter((line) =>
+  {
     if(line.geometry.type === 'LineString' || line.geometry.type === 'MultiLineString') return true;
   });
   return lines;
 }
 
-function filter(road, type = "Polygon") {
-  if(type === "Polygon") {
+function filter(road, type = "Polygon") 
+{
+  if(type === "Polygon") 
+  {
     var area = turf.area(road, 'kilometers');
-    if(area < 350) {
+    if(area < 350) 
+    {
       return true;
-    } else {
+    } 
+    else 
+    {
       return false;
     }
   }
-  else {
+  else 
+  {
     var length = turf.lineDistance(road, 'kilometers');
-    if (length < 0.03) {
+    if (length < 0.03) 
+    {
       return true;
-    } else {
+    } 
+    else 
+    {
       return false;
     }
   }
 }
 
-function getNewHash(featcoords, hashedcoords) {
+function getNewHash(featcoords, hashedcoords) 
+{
   NotNewHashCodeGenerationException.prototype = Object.create(Error.prototype);
   NotNewHashCodeGenerationException.prototype.name = "NotNewHashCodeGenerationException";
   NotNewHashCodeGenerationException.prototype.constructor = NotNewHashCodeGenerationException;
 
   var hashresult = "";
   let hashnb = 17;
-  try {
+  try 
+  {
       hashresult = featcoords + hashedcoords;
       /*
       for(let i = 0; i < hashedcoords.length;i++) {
@@ -252,37 +305,44 @@ function getNewHash(featcoords, hashedcoords) {
       return hashresult;
       
 
-  }catch(err){
+  }
+  catch(err)
+  {
     err = new NotNewHashCodeGenerationException("Data are invalid or hashing process doesn't work please report to getNewHash function");
     console.log(err.toString());
   }
 }
 
 // Comparing a tag from two different sources and returns true if those are different
-function CompareByTags(refTag, sourceTag) {
+function CompareByTags(refTag, sourceTag) 
+{
   if(refTag !== sourceTag) return true;
   else return false;
 }
 
 // Comparing a tag and returns true if this tag has been put to yes
-function CompareByTag(refTag) {
+function CompareByTag(refTag) 
+{
   if(refTag === "yes") return true;
   else return false;
 }
 
 // Returns true if there is cobblestone in the feature
-function CompareCobblestone(refTag) {
+function CompareCobblestone(refTag) 
+{
   if (refTag === "sett" || refTag === "unhewn_cobblestone" || refTag === "cobblestone") return true;
   else return false;
 }
 
-function NotNewHashCodeGenerationException(message) {
+function NotNewHashCodeGenerationException(message) 
+{
     this.message = message;
     if("captureStackTrace" in Error) Error.captureStackTrace(this, NotNewHashCodeGenerationException);
     else this.stack = (new Error()).stack;
-  }
-function NotConfirmedHashCodeException(message) {
+}
+function NotConfirmedHashCodeException(message) 
+{
     this.message = message;
     if("captureStackTrace" in Error) Error.captureStackTrace(this, NotConfirmedHashCodeException);
     else this.stack = (new Error()).stack;
-  }
+}
