2a289158d4db152b167705a16f87d5f7fa6b50c8
xivk <ben@itinero.tech>
2018-07-25 22:40:58 +0200

    Output some more info during script on what is running.

--- docker/run.sh
+++ docker/run.sh
@@ -5,23 +5,28 @@ file="$(pwd)/host-share"
 rm -rf $file/*
 
 # gets data for osm and urbis, cuts out brussels for osm
+echo "Running data tool:"
 cd data-tool
-sudo docker run -v $file:/sharedfolder road-completion-getdata
+#sudo docker run -v $file:/sharedfolder road-completion-getdata
 #sudo docker run -v /home/antoine/road-completion/docker/host-share:/sharedfolder road-completion-getdata
 
+echo "Running tag process:"
 cd ../tagprocess
 sudo docker run -v $file:/sharedfolder road-completion-tagprocess
 
 # convert data from osm and urbis to comparable mbtiles
+echo "Running tag conversion:"
 cd ../convert
 sudo docker run -v $file:/sharedfolder road-completion-convert
 
 # starts index.js for the converted mbtiles
+echo "Running difference tool:"
 cd ../difference
 sudo docker run -v $file:/usr/src/app/sharedfolder road-completion-difference
 
 # convert to data usable by the frontend
 # when output.geosjon is not found, the convert script does not run, now it is generated so it will run
+echo "Preparing output:"
 cd ../mb-util
 sudo docker run -v $file:/sharedfolder road-completion-convert
 sudo docker run -v $file:/sharedfolder road-completion-mbutil
