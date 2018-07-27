1aed2d64510987c10b63b22b7e4cd4d2ae07fd5d
xivk <ben@itinero.tech>
2018-07-27 18:11:46 +0200

    Added wegenregister source.

---
+++ docker/data-tool/wegenregister/get-data.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# this downloads and converts the wegenregister data to a format that can be used by tippecanoe and tile-reduce.
+
+# https//overheid.vlaanderen.be/wegenregister-stand-van-zaken
+# https://download.agiv.be/Producten/Detail?id=3810&title=Wegenregister_22_06_2017
+# get wegenregister if not present
+# IMPORTANT: it's possible the URL just changes, horrible right? nothing we can do about it.
+if [ ! -d ./Wegenregister_SHAPE_20180621 ]; then
+wget -O ./Wegenregister_SHAPE_20180621.zip https://downloadagiv.blob.core.windows.net/wegenregister/Wegenregister_SHAPE_20180621.zip
+unzip ./Wegenregister_SHAPE_20180621.zip -d ./
+fi
+
+#convert wegenregister to geojson
+if [ ! -f ./wegenregister.geojson ]; then
+ogr2ogr --config SHAPE_ENCODING "ISO-8859-1" -f "GeoJSON" -s_srs "EPSG:31370" -t_srs "EPSG:4326" -progress ./wegenregister.geojson ./Wegenregister_SHAPE_20180621/Shapefile/Wegsegment.shp
+fi
+
+cp wegenregister.geojson ../../../sharedfolder
